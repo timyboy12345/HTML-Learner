bb0ff362654776355d55fa3a3fb987663adf160b
Tim Arendsen <t.j.m.arendsen@gmail.com>
2026-01-19 13:45:44 +0100

    Changed HTML port

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,7 +2,7 @@ module.exports = {
     apps: [
         {
             name: 'Learner',
-            port: '2014',
+            port: '3014',
             exec_mode: 'cluster',
             instances: 'max',
             script: './.output/server/index.mjs',
